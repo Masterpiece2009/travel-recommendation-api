c09cbba38136ed16244f4b02790017e77f18a30e
Masterpiece2009 <168648866+Masterpiece2009@users.noreply.github.com>
2025-04-11 04:23:29 +0200

    Update main_fixed.py

--- main_fixed.py
+++ main_fixed.py
@@ -175,4 +175,4 @@ async def search_user(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main_fixed:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)

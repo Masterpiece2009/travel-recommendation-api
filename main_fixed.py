7760f125dad1396e45dcdb24df3803747f91995e
Masterpiece2009 <168648866+Masterpiece2009@users.noreply.github.com>
2025-04-26 18:52:07 +0300

    Update main_fixed.py

--- main_fixed.py
+++ main_fixed.py
@@ -4550,30 +4550,21 @@ def simplify_roadmap_to_list(roadmap_data):
     # Prepare message object
     message = None
     if has_warnings:
-        # Include all warnings in an array
+        # Always include all warnings in an array
         warnings = roadmap_data["warnings"]
-        if len(warnings) == 1:
-            # If there's only one warning, use it directly
-            warning = warnings[0]
-            message = {
-                "type": "warning",
-                "warning_type": warning.get("type", "general"),
-                "message": warning.get("message", "Warning"),
-                "is_warning": True
-            }
-        else:
-            # If there are multiple warnings, include them all
-            message = {
-                "type": "warnings",
-                "warnings": [
-                    {
-                        "warning_type": w.get("type", "general"),
-                        "message": w.get("message", "Warning")
-                    } for w in warnings
-                ],
-                "message": f"There are {len(warnings)} warnings for this roadmap.",
-                "is_warning": True
-            }
+        warnings_list = [
+            {
+                "warning_type": w.get("type", "general"),
+                "message": w.get("message", "Warning")
+            } for w in warnings
+        ]
+        
+        message = {
+            "type": "warnings",
+            "warnings": warnings_list,
+            "count": len(warnings_list),
+            "is_warning": True
+        }
     else:
         # Add a positive message when there are no warnings
         message = {
